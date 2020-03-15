bcbfab58e1a98d03e7c45a5bc6470c121668baa0
poifull10 <poifull10@gmail.com>
2020-03-15 16:49:33 +0900

    update ci

--- .ci/run.sh
+++ .ci/run.sh
@@ -11,7 +11,7 @@ FILE_NAME=dataset.zip
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o ${FILE_NAME}
-unzip -o dataset.zip
+unzip -o dataset.zip -d data
 
 # build
 mkdir build
